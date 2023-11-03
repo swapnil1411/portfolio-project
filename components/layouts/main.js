3a74c51548167d5b1f180c23afe85e50151b0393
swapnil1411 <201801083@daiict.ac.in>
2023-11-03 07:10:36 -0400

    first commit

---
+++ components/layouts/main.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
+
+const Main = ({children,router}) => {
+    return (
+        <Box as="main" pb={8}>
+        <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Swapnil Nanavati - Homepage</title>
+        </Head>
+        <Container maxW="container.md" pt={14}>
+        {children}
+        </Container>
+        </Box>
+    )
+}
