323e5d8552128a7435d8aa227ab463d42ce423de
swapnil1411 <201801083@daiict.ac.in>
2023-11-04 06:15:51 -0400

    vercel deployment

--- components/profile.js
+++ components/profile.js
@@ -1,29 +1,29 @@
 import { Box, Button, Container, Flex, Heading, Image } from '@chakra-ui/react'
-import { useContext } from 'react';
-import MyContext from './context';
+import { useContext } from 'react'
+import MyContext from './context'
 import React, { useState } from 'react'
 const Profile = () => {
-  const [isDarkMode, setIsDarkMode] = useContext(MyContext);
+  const [isDarkMode, setIsDarkMode] = useContext(MyContext)
 
   return (
     <Container
-    background={isDarkMode?'#505483':'#72899B'}
-    color={isDarkMode?'gray.100':'gray.800'}
+      background={isDarkMode ? '#505483' : '#72899B'}
+      color={isDarkMode ? 'gray.100' : 'gray.800'}
       minW={'100%'}
       minH={'calc(85vh)'}
       my={4}
       rounded={6}
     >
-      <Flex  flexDirection={'row'} alignItems={'center'}>
-        <Box flexBasis={'100%'} >
-          <Heading as="h1" fontSize="9xl" >
+      <Flex flexDirection={'row'} alignItems={'center'}>
+        <Box flexBasis={'100%'}>
+          <Heading as="h1" fontSize="9xl">
             Web
             <br /> Developer
           </Heading>
-          <Heading as="h2" fontSize="xl" >
+          <Heading as="h2" fontSize="xl">
             ------ Hi, I'm Swapnil Nanavati
           </Heading>
-          <Heading as="h2" fontSize="md" >
+          <Heading as="h2" fontSize="md">
             a web developer based in India
           </Heading>
 
@@ -32,11 +32,9 @@ const Profile = () => {
           </Button>
         </Box>
 
-         <Box>
-      <Image src="profile.jpg" 
-      boxSize={'70%'}
-      alt='Swapnil Nanavati'  />
-        </Box> 
+        <Box>
+          <Image src="profile.jpg" boxSize={'70%'} alt="Swapnil Nanavati" />
+        </Box>
       </Flex>
     </Container>
   )
